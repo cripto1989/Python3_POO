4aca2f582562e7eedfa3afc59f6c7d42c66c6cc9
Arturo Herrera <cripto1989@gmail.com>
2016-08-04 11:55:29 -0500

    Agrega ejemplo de metodo __doc__

---
+++ Metodos extras/Principal.py
@@ -0,0 +1,7 @@
+class Principal():
+    """
+    Esta es la documentacion de la clase
+    """
+
+# El metodo __doc__ permite mostrar la documentacion que se le haya escrito a la clase
+print(Principal.__doc__)
